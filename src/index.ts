4d0e8a022395c7af24298780c2d16acd9a5f76b7
danielpqb <danielpqb@gmail.com>
2025-01-22 17:18:03 -0300

    release: InputSelect

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { InputDate } from "./components/InputDate";
 export { InputImage } from "./components/InputImage";
 export { InputSelectAutocomplete } from "./components/InputSelectAutocomplete";
 export { InputText } from "./components/InputText";
+export { InputSelect } from "./components/InputSelect";
